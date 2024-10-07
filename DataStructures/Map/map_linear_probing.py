b9416ef53ecf5fa7f004d36468823ad8549978f7
FruzeAMil <mc.rodriguezp12@uniandes.edu.co>
2024-10-07 15:57:40 -0500

    mis funciones sin find slot

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -1,5 +1,27 @@
+import random
+from DataStructures.Map import map_functions as mp
+from DataStructures.List import array_list as al
+
 def new_map(num_elements, load_factor, prime=109345121):
-    pass
+    
+    capacity = mp.next_prime(int(num_elements // load_factor))
+    scale = random.randint(1, prime - 1)
+    shift = random.randint(0, prime - 1)
+    table = [] * capacity
+
+    map_structure = {
+        'prime': prime,
+        'capacity': capacity,
+        'scale': scale,
+        'shift': shift,
+        'table': table,
+        'current_factor': 0,
+        'limit_factor': load_factor,
+        'size': 0,
+        'type': 'PROBING'
+    }
+
+    return map_structure
 def put(my_map, key, value):
     pass
 def contains(my_map, key):
@@ -16,12 +38,25 @@ def key_set(my_map):
     pass
 
 def value_set(my_map):
-    pass
+    return_list = al.new_list()
+    if size(my_map) > 0:
+        for i in range(my_map["capacity"]):
+            if is_available(my_map, i) is True:
+                al.add(return_list, my_map["table"][i]["value"])
+    return return_list
 def find_slot(my_map, key, hash_value):
     pass
 def is_available(table, pos):
-    pass
+    if table["elements"][pos]["key"] is None or table["elements"][pos]["key"] == "__EMPTY__":
+        return True
+    else:
+        return False
 def rehash(my_map):
     pass
 def default_compare(key, element):
-    pass
+    if key == element["key"]:
+        return 0
+    elif key < element["key"]:
+        return 1
+    elif key > element["key"]:
+        return -1
