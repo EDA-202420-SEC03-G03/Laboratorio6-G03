7c7c3598da86b8c1fd88f99d54a7cdeab6be697c
Pedroalej0 <p.angaritar@uniandes.edu.co>
2024-10-07 19:52:51 -0500

    mis funciones

---
+++ DataStructures/Map/map_linear_probing.py
@@ -0,0 +1,51 @@
+from DataStructures.List import array_list as al
+def new_map(num_elements, load_factor, prime=109345121):
+    pass
+
+def put(my_map, key, value):
+    pass
+
+def contains(my_map, key):
+    pass
+
+def get(my_map, key):
+    pass
+
+def remove(my_map, key):
+
+    pass
+
+#mias
+def size(my_map):
+    return al.size(my_map["table"])
+
+def is_empty(my_map):
+    return al.is_empty(my_map["table"])
+        
+        
+    
+
+def key_set(my_map):
+    a=al.new_list
+    for i in my_map["table"]["elements"]:
+        al.add_last(i[0])
+    return a    
+        
+        
+        
+    
+#camila
+def value_set(my_map):
+    pass
+
+def find_slot(my_map, key, hash_value):
+    pass
+
+def is_available(table, pos):
+    pass
+
+def rehash(my_map):
+    pass
+
+def default_compare(key, element):
+    pass
